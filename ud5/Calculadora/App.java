70353939f494cdd415f2d4b9b7ae2fb5559792f1
a21dylanap <a21dylanap@iessanclemente.net>
2023-11-03 11:31:30 +0100

    dia3

--- ud5/Calculadora/App.java
+++ ud5/Calculadora/App.java
@@ -2,7 +2,7 @@ package ud5.Calculadora;
 
 public class App {
  
-    private Menu menu;
+   private Menu menu;
     private Calculadora calculadora;
 
     public App(){
@@ -10,5 +10,41 @@ public class App {
         calculadora = new Calculadora();
     }
 
+    public void start(){
+        double numero = 0.0;
 
+        while (true) {
+            int seleccion = menu.getSeleccionValida();
+
+            switch (seleccion) {
+                case 0:
+                    calculadora.reset();
+                    break;
+
+                case 1:
+                    numero = menu.getNumeroValido();
+                    calculadora.sumar(numero);
+                    break;
+
+                case 2:
+                    numero = menu.getNumeroValido();
+                    calculadora.restar(numero);
+                    break;
+
+                case 3:
+                    numero = menu.getNumeroValido();
+                    calculadora.multiplicar(numero);
+                    break;
+                
+                case 4:
+                    numero = menu.getNumeroValido();
+                    calculadora.dividir(numero);
+                    break;
+                default:
+                    break;
+            }
+
+            calculadora.imprimir();
+        }
+    }
 }
